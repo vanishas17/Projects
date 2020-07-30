f063a851947f2128655e59e9ca39902d67c68ae7
vanishas17 <vanisha.sawant@gmail.com>
2020-07-30 14:16:07 -0400

    Initial Commit

---
+++ Assignment/Model/Trade.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment.Models
+{
+    class Trade
+    {
+        [JsonProperty("sym")]
+        public string StockName { get; set; }
+
+        [JsonProperty("P")]
+        public double Price { get; set; }
+
+        [JsonProperty("Q")]
+        public double QuantityTraded { get; set; }
+
+        [JsonProperty("TS2")]
+        public Int64 TimeStamp { get; set; }
+
+        public DateTime TradeInDateTime
+        {
+            get
+            {
+                long nanoseconds = TimeStamp;
+                DateTime epocTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+                DateTime result = epocTime.AddTicks(nanoseconds / 100);
+
+                return DateTime.ParseExact(result.ToString("yyyy-MM-dd HH:mm:ss"), "yyyy-MM-dd HH:mm:ss", null);
+            }
+        }
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using Assignment.Models;
+using Newtonsoft.Json;
+
+namespace Assignment
+{
+    class Program
+    {
+        static BufferBlock<Trade> bufferBlock = new BufferBlock<Trade>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running OHLC Application!");
+            Task.Run(Reader);
+        }
+
+        static async Task Reader()
+        {
+            var myJsonString = File.ReadAllText(@"TradeFeed\trades1.json");
+            var tradeList = JsonConvert.DeserializeObject<List<Trade>>(myJsonString);
+
+            foreach (var item in tradeList)
+            {
+                bufferBlock.Post<Trade>(item);
+            }
+        }
+    }
+}
